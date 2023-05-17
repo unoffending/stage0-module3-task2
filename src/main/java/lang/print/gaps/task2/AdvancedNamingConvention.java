bd2024b3f3b371b6a1b17a0279c6144beaae4e9f
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-17 15:59:45 +0600

    Update AdvancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int adultAge = 18;
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
